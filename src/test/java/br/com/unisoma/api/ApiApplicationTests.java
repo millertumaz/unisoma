af43d42a409de3533dcae1a7ba78c85290bf75b9
Miller Tumaz <miller.tumaz@hotmail.com>
2023-06-12 23:21:52 -0300

    first commit

---
+++ src/test/java/br/com/unisoma/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unisoma.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
